109e41b261008bbf6d9749f3d71a4514d836cfbe
Basit Shehram <basit@shehram.com>
2024-03-16 18:10:09 -0700

    added local backend example

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 terraform {
   required_version = ">= 1.7.5"
 
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket" # Replace with your S3 bucket name
-    key            = "path/to/your/state/file"     # Path within the bucket to your state file
-    region         = "us-west-2"                   # AWS region for the S3 bucket
-    encrypt        = true                          # Enable encryption at rest
-    dynamodb_table = "your-lock-table"             # DynamoDB table for state locking, replace with your table name
-  }
+#   backend "s3" {
+#     bucket         = "your-terraform-state-bucket" # Replace with your S3 bucket name
+#     key            = "path/to/your/state/file"     # Path within the bucket to your state file
+#     region         = "us-west-2"                   # AWS region for the S3 bucket
+#     encrypt        = true                          # Enable encryption at rest
+#     dynamodb_table = "your-lock-table"             # DynamoDB table for state locking, replace with your table name
+#   }
+
+#   backend "local" {
+#     path = "path/to/your/terraform.tfstate"        # Path to your local state file
+#   }
+
 }
 
 provider "aws" {
